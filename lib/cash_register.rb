cfec3cf04eebc29c00e780b8816a2af8eb59b068
David masterson <david73180@gmail.com>
2018-10-16 03:12:53 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,35 @@
+require 'pry'
 
+class CashRegister
+
+  attr_accessor :quantity, :price, :total, :title, :discount
+
+  def initialize(discount = nil)
+    @total = 0
+    @discount = discount
+
+  end
+
+  def total(total)
+    @total = total.to_f
+  end
+
+  def add_item(title, price)
+    register = CashRegister.new(title, price)
+    @total = @total + price
+    @quantity = nil
+
+  end
+
+
+  def apply_discount
+    @discount = @discount.to_f / 100
+    register = CashRegister.new(@discount)
+    @discount = (total * @discount)
+    @total = @total - @discount
+    discount_message = "After the discount, the total comes to $#{@total.to_i}."
+    return discount_message
+
+  end
+
+end
