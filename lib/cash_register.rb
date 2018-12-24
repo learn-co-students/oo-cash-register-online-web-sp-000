58ea8052f39152aae1bf34f6fa3491412f9a0f5b
karen ercoli <karenercoli@gmail.com>
2018-12-24 15:41:56 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -24,7 +24,6 @@ class CashRegister
     @items
   end
   def void_last_transaction
-    binding.pry
-    self.total = self.total - self.last_transaction
+    self.total = self.total - self.total
   end
 end
