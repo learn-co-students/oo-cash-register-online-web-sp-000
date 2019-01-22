a900fef6895aa8158f04149ce0155e289798179f
Diana Badas <diana.badas@gmail.com>
2019-01-22 16:54:18 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+class CashRegister
+  attr_accessor :total, :discount
 
+  def initialize(total = 0, discount = nil)
+    @total = total
+    @discount = discount
+  end
+
+  def self.total
+    @total
+  end
+
+  def add_item
+
+  end
+
+  def apply_discount
+
+  end
+
+end
