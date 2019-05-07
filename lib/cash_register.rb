eaaa674bcbf1ff494786bb22a228816e4a6f1175
Ramon Chavez <rachavez1974@gmail.com>
2019-05-07 02:06:00 -0400

    Finish o-cash-register-online-web-sp-000

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
+class CashRegister
+  attr_accessor :total, :discount, :items, :last_transaction
 
+  def initialize(discount = nil)
+    @total = 0
+    @discount = discount
+    @items = []
+    @last_transaction = 0
+  end
+
+  def add_item(title, price, quantity = 1)
+    @last_transaction = price * quantity
+    @total += @last_transaction
+    save_items(title, quantity)
+  end
+
+  def apply_discount
+    if @discount.nil?
+      "There is no discount to apply."
+    else
+      @total = @total - (@total * (@discount.to_f/100))
+      "After the discount, the total comes to $#{@total.to_i}."
+    end
+  end
+
+  def void_last_transaction
+    @total -= @last_transaction    
+  end
+  private
+
+  def save_items(title, quantity)
+    until quantity == 0
+      @items << title
+      quantity -= 1
+    end
+  end
+end
