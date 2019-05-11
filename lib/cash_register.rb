b2347e4f0bd25c406f9ce187dc370e49f14c2cb8
Andrew Silva <andrewsilva9@gmail.com>
2019-05-11 01:16:38 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
+class CashRegister
+  attr_accessor :total, :discount, :cost, :item
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  def add_item(item, price, quantity = 1)
+    @price = price
+    @total += price * quantity
+    if quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << item
+        counter += 1
+      end
+    else
+      @items << item
+    end
+  end
+  def apply_discount
+    discount = (price * quantity)/100
+    @total -= discount
+  end
+
+end
