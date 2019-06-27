6c3ade363a041bb1fdffb75f0b661665d9a7e639
Corey H <c.l.halicke@gmail.com>
2019-06-27 02:42:15 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
 
+class CashRegister
+  attr_accessor :total
+
+  def initialize(total = 0)
+    @total = total
+  end
+
+  def discount
+    employee_discount = 20
+    employee_discount
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+  end
+end
