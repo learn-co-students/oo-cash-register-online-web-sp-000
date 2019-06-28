5894797cc8d53d9529073623cb135b1184b90f57
Kimberly Rouse <ksrouse94@gmail.com>
2019-06-28 23:49:05 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,8 +2,8 @@ class CashRegister
   
   attr_accessor :total, :discount
   
-  def initialize(total = 0, discount = 20)
-    @total = total
+  def initialize(discount = 20)
+    @total = 0
     @discount = discount
   end
   
@@ -13,10 +13,12 @@ class CashRegister
   
   def apply_discount
     #applying discounts --> turn discount into decimal value --> multiply the total and discount to determine the amount to be removed --> subtract the amount to be removed from the current total 
-    if @discount > 0 
+    
+    if @discount != 0 
       discount = (100-@discount)/100.0
       @total = @total*discount
-      puts "After the discount, the total comes to $#{@total}."
-    end 
+      return "After the discount, the total comes to $#{@total.to_i}."
+    end  
+    
   end
 end  
