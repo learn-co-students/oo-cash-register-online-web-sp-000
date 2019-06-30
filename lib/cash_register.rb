47e47b9879b4c5a705ec83ab3b10419ec6ce8033
juan siu <juan.siu@gmail.com>
2019-06-30 20:25:48 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
-
+class CashRegister
+  attr_accessor :total, :cash_register_with_discount, :cash_register, :discount, :employee_discount
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    
+  end
+  
+  def add_item(item_title, item_price, optional_qty = 1)
+    self.total = @total + item_price * optional_qty
+    
+  end
+    
+  def apply_discount
+    @cash_register_with_discount = self.total - @discount/100*@total
+    "After"
+    
+  end
+    
+  
+end
