ce780d6c2a8d49c5d0bb280f13d422478117c525
Connor Cowan <ccowan003@comcast.net>
2019-07-01 22:16:47 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+class CashRegister
 
+  attr_accessor :total
+
+  def initialize(total = 0)
+
+    @total = total
+  end
+
+  def add_item(title, price)
+
+  end
+
+  def apply_discount
+
+  end
+
+  def items
+
+  end
+
+  def void_last_transaction
+    self.@total.clear
+  end
+end
+# to call an instance method inside another instance method, we use the self
