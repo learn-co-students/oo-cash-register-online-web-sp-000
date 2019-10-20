86fbc072631ed789bfc17f95d268a7364786ec91
Anthony Tilelli <anthony@tilelli.me>
2019-10-20 03:45:31 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -27,7 +27,7 @@ class CashRegister
   end
   
   def void_last_transaction
-    product = @pending_purchases.pop
-    @total -= product[1] * product[2]
+    voided_product = @pending_purchases.pop
+    @total -= voided_product[1] * voided_product[2]
   end
 end
