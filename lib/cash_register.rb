b0e8281557725d12bd0954bef58bd65349535563
Anthony Tilelli <anthony@tilelli.me>
2019-10-20 03:11:33 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,15 @@
+require "pry"
 class CashRegister
-  attr_accessor :total
+  attr_accessor :total, :discount
   
-  def initialize()
+  def initialize(employee_discount = 0)
      @total = 0
+     @discount = employee_discount
+     @pending_purchases = []
+  end
+  
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+    @pending_purchases <<  [title, price, quantity]
   end
 end
