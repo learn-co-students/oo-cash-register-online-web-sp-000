ed2a8a773239b9e23c102775ed20971c384ee325
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-22 20:09:30 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,10 @@
+class CashRegister
+    attr_accessor :total :discount
+  def initialize(total = 0, discount = 20)
+    @total = total
+    @discount = discount
+  end
 
+  def total
+    @total
+  end
