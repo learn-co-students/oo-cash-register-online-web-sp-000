c2895d66bb70720a3d8ab453be6daed4901d0024
Chad Lengefeld <chadlengefeld@gmail.com>
2019-11-23 21:33:20 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,37 +1,54 @@
 require "pry"
 
 class CashRegister
-  attr_accessor :total, :add_item, :quantity, :new_total
+  attr_accessor :total, :discount, :add_item, :quantity, :title, :price, :new_total
 
-  def initialize(discount = 0)
+  def initialize(discount = 0 )
     @total = 0
     @discount = discount
-  #  options[:discount] ||=   @total.to_i - (@total.to_i * 0.2)
-
   end
 
 
   def add_item(title, price, quantity = 1)
 #binding.pry
+#unless @title = nil
     @total = @total + (price * quantity)
-  #  new_total = (@total + price) * quantity
-  #  @total << new_total
+    @title = title
+
   end
+
 
   def apply_discount
   # binding.pry
-if  @discount == 0
-  puts "There is no discount to apply."
-
-
-else
-binding.pry
-    @total + @discount
-      puts "After the discount, the total comes to #{@discount}"
+    if  @discount == 0
+        return "There is no discount to apply."
+      else
+          @total -= ((@discount.to_f / 100.0) * @total)
+      return "After the discount, the total comes to $#{@total.to_i}."
+      end
     end
-  end
-     # @new_total = @total - (@total * 0.2)
-  #self.total  - (self.total * 0.2)
 
 
+  def items
+ item = []
+#binding.pry
+
+unless @title == nil
+item << @title
+
+end
+
+end
+
+  def void_last_transaction
+if @price == nil
+    @total = 0.0
+#binding.pry
+else
+  self - (@price * @quantity)
+end
+
+
+end
+#  @total  = 0.0
 end
