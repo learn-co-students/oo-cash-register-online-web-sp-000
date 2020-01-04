736dbfc0f388cccd1d587895b21164388f1a2e30
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-04 16:55:50 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
+# Cash register can:
+# Add items of varying quantities and prices
+# Calculate discounts
+# Keep track of what's been added to it
+# Void the last transaction
 
+class CashRegister
+
+  attr_accessor :total, :discount, :last_transaction_item, :items
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+
+end
