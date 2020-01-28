27ac8bf647e7d9d7b4cc336ed81c338ff96588f1
Taylor Mills <taylormills24@yahoo.com>
2020-01-28 10:12:53 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class CashRegister 
   attr_accessor :discount, :item, :last_transaction, :total
 
@@ -15,29 +16,28 @@ class CashRegister
   def add_item_2(cost, name, number = 1)
     self.total = cost * number
 
-  def apply_discount 
-  # the cash register was initialized with an employee discount:  
-    # applies the discount to the total price
-    # returns success message with updated total
-    # reduces the total
-    
-  # the cash register was *NOT* initialized with an employee discount:
-    # returns a string error message that there is no discount to appply
-  end 
+  def apply_discount
+    if @discount > 0
+      @to_take_off = (price * discount)/100
+      @total -= @to_take_off
+      return "After the discount, the total comes to $#{total}."
+    else
+      return "There is no discount to apply."
+    end
+  end
   
   def items 
-  # (self).items!?
-  # returns an array containing all items that have been added
+  tester = CashRegister.new 
+tester.add_item(4, "carrots", 4)
   end
   
   def void_last_transaction
-  # (cash_register.total).to eq(0.99) 
+  @total -= @price 
   end 
   
 end 
 
-tester = CashRegister.new 
-tester.add_item(4, "carrots", 4)
+
 
 
   
