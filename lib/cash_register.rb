25080972fec7f4a22c1edca5859c3789ed73d96f
Doak Covington <doakcovington@protonmail.com>
2020-02-10 04:39:41 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
+#WDGTR
+require 'pry'
+class CashRegister
+  attr_accessor :total, :discount, :price
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = 20
+  end
+
+  def add_item(item, price, quantity = 1)
+    @price = price
+    @total += @price * quantity
+  end
+
+  def apply_discount
+    if @discount > 0
+      employee_discount = (@discount / 100.0) * @total
+      new_total = @total - employee_discount
+      @total = new_total.to_i
+      "After the discount, the total comes to $#{@total}."
+    else
+      "There is no discount to apply"
+    end
+  end
+
+end
