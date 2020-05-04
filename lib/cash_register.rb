7c9b1963fb88ebf1786e67e4d10e68bda67cc913
santiago molinari <santomolinari@gmail.com>
2020-05-04 16:49:43 +0200

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,47 +1,50 @@
 
 class CashRegister
 
-    # attr_accessor :total, :discount
+    attr_accessor :total, :discount, :quantity, :last_transaction, :items
 
-    # def initialize(discount=0)
-    #     @total = 0
-    #     @discount = discount
-    
-    # end
-
-    # def add_item(title, price, quantity=0)
-    #     @total += price
-
-    # end
-
-    attr_accessor :items, :discount, :total, :last_transaction
-
-  def initialize(discount=0)
-    @total = 0
-    @discount = discount
-    @items = []
-  end
-
-  def add_item(title, amount, quantity=1)
-    self.total += amount * quantity
-    quantity.times do
-      items << title
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @items = []
+        
     end
-    self.last_transaction = amount * quantity
-  end
 
-  def apply_discount
-    if discount != 0
-      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
-      "After the discount, the total comes to $#{self.total}."
-    else
-      "There is no discount to apply."
+    #add_item should accepts a title and a price and increase de total
+    # acceptes an optional quantity
+    # keeps track of previous total.
+
+    def add_item(title, price, quantity=1)                                           
+        self.total += price * quantity
+        quantity.times do
+            items << title 
+        end
+        self.last_transaction = price * quantity
+
     end
-  end
 
-  def void_last_transaction
-    self.total = self.total - self.last_transaction
-  end
+    def apply_discount
+        if self.discount > 0
+            self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+            "After the discount, the total comes to $#{self.total}."
+            #applies discount to the total price
+                #
+            #returns success message with uploaded total
+            #reduces the total.
+
+        else # if none discount
+            #returns a string error message that there is no discount to apply
+            "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        self.total = self.total - self.last_transaction
+    end
+
+    def void_last_transaction
+        self.total = self.total - self.last_transaction
+      end
 
 
 
