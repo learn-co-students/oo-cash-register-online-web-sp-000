250b7ec49b8b39235925d9a1efea15011551c666
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-27 22:56:31 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,32 +1,40 @@
 class CashRegister
-  attr_accessor :total, :discount, :price, :items, :last_transaction
+  attr_accessor :total, :discount, :items, :last_transaction
+#attr_accessor creates two methods, getter and setter
+#will this value change and will I have to change it?
+#only has to do with the class
 
-  def initialize(discount = 0)
-    @total = 0
+  def initialize(discount = 0) #constructor
+    @total = 0 #Starts at 0. Instance variables can be shared across all the methods inside the class
     @discount = discount
-    @items = []
+    @items = [] #starts at an empty list.
   end
+#.new = new instance, calls for the initialize
 
   def add_item(item, price, quantity = 1)
-    @price = price
-    @total += price * quantity
+    @price = price #since it is the only one, doesn't have to be an instance variable.
+    self.total += price * quantity #using the writer method instead of creating a local variable
     if quantity > 1
       counter = 0
       while counter < quantity
-        @items << item
+        items << item #doesn't need to be @items
         counter += 1
       end
     else
-      @items << item
+      items << item
     end
-    @last_transaction = price * quantity #pass by reference or pass by value
-    @total
+    self.last_transaction = price * quantity #pass by reference or pass by value
   end
 
+  # quantity.times do  (lines 17-25)
+  # items << item
+  #   end
+
+
   def apply_discount
-    if @discount > 0
-      @to_take_off = (price * discount)/100
-      @total -= @to_take_off
+    if discount > 0    #self. retrieves, no need for @
+      to_take_off = (total * discount)/100 #to_take_off local not instance, only in this method
+      self.total -= to_take_off
       return "After the discount, the total comes to $#{total}."
     else
       return "There is no discount to apply."
@@ -34,7 +42,7 @@ class CashRegister
   end
 
 def void_last_transaction()
-  @total -= @last_transaction
+  self.total -= last_transaction #self. resetting a value writer!
 end
 
 
