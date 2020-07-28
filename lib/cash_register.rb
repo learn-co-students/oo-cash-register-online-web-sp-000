6756191292734b0fa60a0ae0306f9312f772ea40
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-28 20:15:43 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
+require 'pry'
+ class CashRegister
 
+  attr_accessor :total, :discount, :items, :last_total
+
+  def initialize(discount=0)
+    @total = 0.0
+    @last_total = 0.0
+    @discount = discount
+    @items = []
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(title,price,quantity=1)
+  @items.fill(title,@items.size,quantity)
+  @last_total = @total
+  @total += price*quantity
+  end
+
+  def apply_discount
+    if @discount > 0
+      @total = @total - @discount / 100.0 * @total
+      "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    @total = @last_total
+  end
+end
