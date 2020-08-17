7e64d4dff6db0cc4863f352f34c2cd541b459941
jasonronalddavis <jdavis@c.ringling.edu>
2020-08-17 18:34:36 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
 
+class CashRegister
+
+    attr_accessor :total, :discount
+
+
+    def initialize(discount = 0)
+    @discount = discount
+    @total = 0
+    end
+
+
+    def apply_discount
+      self.total.to_f % 20 == 0  
+        if self.total == false
+            puts "There is no discount to apply."
+        end
+    end
+
+    def add_item(title, price, quantity=1)
+
+       @total = price * quantity + @total
+    
+    end
+end
