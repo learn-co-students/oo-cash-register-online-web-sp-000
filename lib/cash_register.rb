d0e0ca3890221a6984007873e8f8838680b4f199
jasonronalddavis <jdavis@c.ringling.edu>
2020-08-18 00:21:12 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,25 +1,52 @@
 
 class CashRegister
 
-    attr_accessor :total, :discount
+    attr_accessor :total, :discount, :title
 
 
     def initialize(discount = 0)
     @discount = discount
     @total = 0
+    @items = []
     end
 
 
     def apply_discount
-      self.total.to_f % 20 == 0  
-        if self.total == false
-            puts "There is no discount to apply."
+        if discount != 0
+            self.total = (total * (100.0 - discount.to_f) / 100)
+
+            "After the discount, the total comes to $#{self.total.to_i}."
+        else 
+            "There is no discount to apply."
         end
+        
     end
 
     def add_item(title, price, quantity=1)
 
        @total = price * quantity + @total
+
+        quantity.times { @items << title } #not all methods can take in an argumentn and be used in this syntax #method.times() proper way = method.times{}
+       
+        @last_item = price * quantity
+    end
+
+
+    	
+
+
+
+
+
+    def items
+        @items
     
+    end
+
+
+    def void_last_transaction
+        self.total -= @last_item
+        self.total
+
     end
 end
