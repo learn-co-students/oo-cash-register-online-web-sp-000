825abf76ec87f54953e162e68f0c8797232a8c17
Kelly Hesterly <kelly.hesterly@gmail.com>
2020-10-09 21:08:43 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+class CashRegister
 
+attr_accessor  :total, :discount
+
+def initialize(total = 0, discount = 20)
+@total = total
+@discount = discount
+end
+
+def add_item(title, price, quantity = 1)
+  if quantity > 1
+quantity * price = total
+else
+  price = total
+end
+end
+
+end
