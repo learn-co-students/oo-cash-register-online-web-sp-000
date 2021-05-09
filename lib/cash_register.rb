6edd4bb602c570a137fabe6792ac98b90c815afe
Drakesonozaki <josephortizbw@gmail.com>
2021-05-08 18:38:34 -0600

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
 
-    attr_accessor :total, :price, :discount, :items, :last_transaction
+    attr_accessor :total, :discount, :last_transaction
 
     def initialize(discount=0)
         @total = 0
@@ -8,11 +8,20 @@ class CashRegister
         @items = []
     end
 
+    def items=(list)
+        @items = list
+    end
+    
+    def items
+        @items
+    end
+
     def add_item(title, price, quantity=1)
         @price = price
         @total += (price * quantity)
-        quantity.times discount
-        items << title
+        quantity.times do
+        @items << title
+        end
         @last_transaction = (price * quantity)
     end
 
@@ -29,4 +38,4 @@ end
 def void_last_transaction
     @total -= @last_transaction
 end
-
+end
